0d753e08d1278a5179adfc7ccc7269a5a750557a
Daniel Otieno <danielotieno.ke@gmail.com>
2021-04-08 12:33:33 +0300

    chore:setup the file structure

---
+++ components/Layout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Layout = () => {
+  return (
+    <div>
+      <h1>This is a layout page</h1>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ pages/events.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EventsPage = () => {
+  return (
+    <div>
+      <h1>This is all events pages</h1>
+    </div>
+  );
+};
+
+export default EventsPage;
