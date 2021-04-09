7504a6be59c241ec9495c835849ff888322e62ef
Daniel Otieno <danielotieno.ke@gmail.com>
2021-04-09 22:49:25 +0300

    chore:create the filtered page

--- components/Nav.js
+++ components/Nav.js
@@ -6,14 +6,14 @@ function Nav() {
     <header className='bg-yellow-800'>
       <div className='container flex flex-col flex-wrap items-center p-5 mx-auto md:flex-row'>
         <Link href='/'>
-          <a className='flex items-center mb-4 font-medium text-gray-900 title-font lg:w-1/2 md:mb-0'>
+          <a className='flex items-center mb-4 font-medium text-gray-900 title-font md:w-1/2 md:mb-0'>
             <h1 className='mt-3 ml-3 font-serif text-3xl font-bold text-white '>
               JS Bits Events
             </h1>
           </a>
         </Link>
 
-        <div className='flex mt-1 lg:justify-end lg:w-1/2 '>
+        <div className='flex mt-1 md:justify-end md:w-1/2 '>
           <Link href='/events'>
             <button className='inline-flex items-center px-6 py-2 mt-4 text-base font-bold text-black bg-white border-0 rounded focus:outline-none hover:bg-gray-200 md:mt-0'>
               Explore All Events
--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,10 +1,54 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import EventList from '../../components/events/EventList';
+import { getFilteredEvents } from '../../data/data';
 
 function FilteredEventsPage() {
+  const router = useRouter();
+
+  const filteredData = router.query.slug;
+
+  console.log(filteredData);
+
+  if (!filteredData) {
+    return (
+      <div>
+        <p>Loading!!!</p>
+      </div>
+    );
+  }
+
+  const [filteredYear, filteredMonth] = filteredData;
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  console.log(numYear);
+
+  if (isNaN(numYear) || isNaN(numMonth) || numMonth > 12 || numMonth < 1) {
+    return (
+      <div>
+        <p>Invalid Filter search</p>
+      </div>
+    );
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (filteredEvents.length === 0 || !filteredEvents) {
+    return (
+      <div>
+        <p>No Events Found</p>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>Filtered Events</h1>
-    </div>
+    <>
+      <EventList events={filteredEvents} />
+    </>
   );
 }
 
